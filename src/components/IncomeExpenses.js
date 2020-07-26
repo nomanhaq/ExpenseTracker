8d62c46addd85d7ff45db4694930a365adb1626e
Abdulrahman ul haq <abdulrahmanhaq.06@gmail.com>
2020-07-26 17:41:54 +0300

    done
    done

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -4,20 +4,9 @@ import {GlobalContext} from '../context/GlobalState';
 
 /////////////
 
-import { makeStyles } from '@material-ui/core/styles';
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    '& > *': {
-      margin: theme.spacing(1),
-      width: theme.spacing(16),
-      height: theme.spacing(16),
-    },
-  },
-}));
+
 
 
 
